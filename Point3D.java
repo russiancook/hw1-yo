d65fce55af1c7db3176d2420846351417d80f43f
russiancook <37901055+russiancook@users.noreply.github.com>
2019-02-24 12:48:32 +0200

    first
    constructors

---
+++ Point3D.java
@@ -0,0 +1,84 @@
+
+/**
+ * Write a description of class Point3D here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Point3D
+{
+    // instance variables - replace the example below with your own
+    private double _x, _y, _z;
+
+    /**
+     * Constructor for objects of class Point3D
+     */
+    public Point3D()
+    {
+        // initialise instance variables
+        _x = 0.0;
+        _y = 0.0;
+        _z = 0.0;
+    }
+    
+    public Point3D(double x, double y, double z)
+    {
+        // initialise instance variables
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+    
+    public Point3D(Point3D otherPoint)
+    {
+        // initialise instance variables
+        if (otherPoint != null)
+        {
+            _x = otherPoint._x;
+            _y = otherPoint._y;
+            _z = otherPoint._z;
+        }
+    }
+    
+    public double getX(Point3D point)
+    {
+        return _x;
+    }
+    
+    public double gety()
+    {
+        return _y;
+    }
+    
+    public double getX()
+    {
+        return _x;
+    }
+    
+    public void setX(double x) 
+    {
+        _x = x;
+    }
+    
+    public void setY(double y) 
+    {
+        _y = y;
+    }
+    
+    public void setZ(double z) 
+    {
+        _z = z;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public double sampleMethod(int y)
+    {
+        // put your code here
+        return _x + _y;
+    }
+}
