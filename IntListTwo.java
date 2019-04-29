c6b9d52a3c693b447f28cf287486a1cab40b0deb
yonahd <47282577+yonahd@users.noreply.github.com>
2019-04-29 09:57:34 +0300

    Create IntListTwo.java
    mmn 15

---
+++ IntListTwo.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class IntListTwo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class IntListTwo
+{
+    private IntNodeTwo _head, _tail;
+    
+    public IntListTwo()
+    {
+        _head = null;
+        _tail = null;
+    }
+    
+    public IntListTwo(IntNodeTwo h, IntNodeTwo t)
+    {
+        _head = h;
+        _tail = t;
+    }
+    
+    public void addNumber(int num)
+    {
+        IntNodeTwo temp = _head;
+        while(temp.getNum() < num )
+        {
+            temp = temp.getNext();
+        }
+        IntNodeTwo newNum = new IntNodeTwo(num, temp, temp.getPrev());
+        newNum.getPrev().setNext(newNum);
+        newNum.getNext().setPrev(newNum);
+    }
+}
